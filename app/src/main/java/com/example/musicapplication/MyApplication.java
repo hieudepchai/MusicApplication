c2aeba7d8c8aa2a0e3a4b641eafb0dcbb47f455b
hieudepchai <hieunguyenvn98@gmail.com>
2019-11-12 07:58:56 +0700

    hieu old

--- app/src/main/java/com/example/musicapplication/MyApplication.java
+++ app/src/main/java/com/example/musicapplication/MyApplication.java
@@ -13,6 +13,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -79,5 +80,6 @@ public class MyApplication extends Application {
         this.listSong = listSong;
     }
     //
+
 
 }
